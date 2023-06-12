a7499d912962252b475504b803e98604b544b296
Kustav <you@example.com>
2023-06-12 14:01:21 +0530

    added check for duplicate insert

--- php/pcbill.php
+++ php/pcbill.php
@@ -23,17 +23,31 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $castid = $_POST["castid"]; 
     $date =  $_POST["bill-date"];
 
+
+
+// Check if $btnbill already exists
+$checkQuery = "SELECT billno FROM btnapp.invoicemain WHERE billno = '$btnbill'";
+$checkResult = mysqli_query($link, $checkQuery);
+
+if (mysqli_num_rows($checkResult) > 0) {
+    // $btnbill already exists, handle the situation as needed (e.g., display an error message)
+    echo "Error: Bill number already exists";
+} else {
     $sql="INSERT INTO btnapp.invoicemain( billno, refbill, cgst, sgst, subtotal, total) VALUES( '$btnbill', '$refbill', '$cgst', '$sgst', '$subtotal', '$ammount')";
+    $sql3 = "UPDATE invoice SET num = num + 1 WHERE type = 'T'";
+    mysqli_query($link, $sql3);
 
     if(mysqli_query($link, $sql)){
-
         $list = json_decode($_POST['list'], true); // decode the JSON string into an array
+        print_r($list);
         foreach($list as $row) {
                $pid = $row['id'];
                 $code = $row['code'];
                 $pmake = $_POST["$code-make"];
-
+            
             if (!empty($pmake)){
+                echo 'dddd';
+
                 $pmodel = $_POST["$code-model"];
                 $prosn = $_POST["$code-serial"];
                 $proqty = $_POST["$code-qty"];
@@ -46,14 +60,14 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $prorate = $_POST["$code-rate"];
 
                 $sql2 = "INSERT INTO btnapp.billpc
-                (castmob, btnbill, date product_id, promake, promod, prosn, proqty, prorate, prowarrenty, prohsn, proammount)
+                (castmob, btnbill, date , product_id, promake, promod, prosn, proqty, prorate, prowarrenty, prohsn, proammount)
                 VALUES('$customer_mob', '$btnbill' , '$date', '$pid', '$pmake', '$pmodel', '$prosn', '$proqty', '$prorate',  DATE_FORMAT(DATE_ADD(CURDATE(), INTERVAL 36 MONTH), '%Y-%m-%d'), ' $prohsn', '$proamm');";
-                $sql3 = "UPDATE invoice SET num = num + 1 WHERE type = 'T'";
+                
                 mysqli_query($link, $sql2);
-                mysqli_query($link, $sql3);
 
 
                 }
         }
     }
+}
 }
