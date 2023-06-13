a978c6113f5444765e3bb92bf56217fe84c97ce5
Kustav <you@example.com>
2023-06-13 12:06:56 +0530

    logo and name

--- navbar.php
+++ navbar.php
@@ -43,7 +43,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <div class="d-flex align-items-center justify-content-between">
   <a href="index.php" class="logo d-flex align-items-center">
     <img src="assets/img/logo.png" alt="">
-    <span class="d-none d-lg-block">Team KEVA</span>
+    <span class="d-none d-lg-block">BTN Techno</span>
   </a>
   <i class="bi bi-list toggle-sidebar-btn"></i>
 </div><!-- End Logo -->
